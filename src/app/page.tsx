f75a40c84c131f6a3b9cbc2b8932a5f775c6fa4a
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-08-20 20:41:59 +0530

    feat(ui): add community numbers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
+import Numbers from "@/components/Numbers";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <About />
+      <Numbers />
     </div>
   );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,18 @@
+import { IconType } from "@icons-pack/react-simple-icons";
+
 export type navLinksType = {
   name: string;
   url: string;
   type: "link" | "button";
   openInNewTab?: boolean;
+}[];
+
+export type numbersType = {
+  name: string;
+  number: number;
+  icon: IconType;
+  color: string;
+  href: string;
 }[];
 
 export interface GeneralComponent extends React.HTMLAttributes<HTMLDivElement> {
