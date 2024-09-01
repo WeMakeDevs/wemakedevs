09fef147f443e31f834543f25f345fb0d20aa1c3
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-02 01:42:18 +0530

    feat: this is working finally lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Numbers from "@/components/Numbers";
+import { fetchHackathonData } from "@/lib/hackathons";
 
-export default function Home() {
+export default async function Home() {
+  const hackathons = await fetchHackathonData();
+  console.log(hackathons);
   return (
     <div>
       <Header />
