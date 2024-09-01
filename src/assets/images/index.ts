42e3278651e5d5e717896a4ff7a3fb6d389b9b5d
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-02 02:07:42 +0530

    fix: move images for better loading

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,4 +2,17 @@ import logo from "./logo.svg";
 import heroBg from "./wemakedevs_hero_bg.png";
 import aboutUfo from "./wemakedevs_about_ufo.svg";
 
-export { logo, heroBg, aboutUfo };
+import hackForGoodCover from "./hack-for-good-cover.png";
+import hackForGoodThumbnail from "./hack-for-good-thumbnail.jpg";
+import codefest2024Cover from "./codefest-2024-cover.png";
+import codefest2024Thumbnail from "./codefest-2024-thumbnail.jpg";
+
+export {
+  logo,
+  heroBg,
+  aboutUfo,
+  hackForGoodCover,
+  hackForGoodThumbnail,
+  codefest2024Cover,
+  codefest2024Thumbnail,
+};
