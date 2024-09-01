9b95ecca0b4a317157c8200b139202402ebc7794
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-02 02:19:36 +0530

    feat: add /hackathons route to display all the hackathons

---
+++ src/app/hackathons/page.tsx
@@ -0,0 +1,42 @@
+import { ViewContainer } from "@/components/ui/view-container";
+import { fetchHackathonData } from "@/lib/hackathon";
+import {
+  HackathonCard,
+  HackathonCardImage,
+  HackathonCardDates,
+  HackathonCardTitle,
+  HackathonCardStatus,
+  HackathonCardDescription,
+} from "@/components/HackathonCard";
+
+const Hackathons = async () => {
+  const hackathons = await fetchHackathonData();
+
+  return (
+    <main className="">
+      <ViewContainer className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {hackathons.map((hackathon, index) => (
+          <HackathonCard key={index} href={`/hackathons/${hackathon.slug}`}>
+            <HackathonCardImage
+              src={hackathon.image.thumbnail}
+              alt={hackathon.title}
+            />
+            <HackathonCardTitle>{hackathon.title}</HackathonCardTitle>
+            <div className="flex justify-between items-center">
+              <HackathonCardStatus status={hackathon.status} />
+              <HackathonCardDates
+                startDate={hackathon.startDate}
+                endDate={hackathon.endDate}
+              />
+            </div>
+            <HackathonCardDescription>
+              {hackathon.description}
+            </HackathonCardDescription>
+          </HackathonCard>
+        ))}
+      </ViewContainer>
+    </main>
+  );
+};
+
+export default Hackathons;
