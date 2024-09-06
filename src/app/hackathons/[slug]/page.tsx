bfe55ee205cef1107f38c2841c7cb49f39ba4f1f
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-07 02:55:30 +0530

    feat: style overview section

--- src/app/hackathons/[slug]/page.tsx
+++ src/app/hackathons/[slug]/page.tsx
@@ -7,10 +7,12 @@ import HackathonContentBody from "@/components/hackathon-content/HackathonConten
 import HackathonContentTitle from "@/components/hackathon-content/HackathonContentTitle";
 import HackathonStatus from "@/components/HackathonStatus";
 import { CustomMDX } from "@/components/mdx-remote";
+import { buttonVariants } from "@/components/ui/button";
 import { ViewContainer } from "@/components/ui/view-container";
 import { fetchHackathonData } from "@/lib/hackathon";
 import { cn } from "@/lib/utils";
 import { Star } from "lucide-react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
@@ -32,24 +34,43 @@ const HackathonPage = async ({ params }: { params: { slug: string } }) => {
       <HackathonCoverImage src={hackathon.image.cover} alt={hackathon.title} />
       <HackathonNav slug={hackathon.slug} page='overview' />
       <ViewContainer>
-        <div className={cn("")}>
-          <div className='bg-foreground/5 border border-foreground/10 rounded-lg px-6 py-8'>
-            <HackathonStatus
-              className='w-fit'
-              startDate={hackathon.startDate}
-              endDate={hackathon.endDate}
-            />
-            <h1 className='text-4xl md:text-6xl font-bold mt-6'>
-              {hackathon.title}
-            </h1>
-            <p className='text-base md:text-lg text-foreground/90 font-medium leading-snug mt-4 tracking-wide'>
-              {hackathon.description}
-            </p>
-            <HackathonDate
-              startDate={hackathon.startDate}
-              endDate={hackathon.endDate}
-            />
-            <p className=''>Prizes worth: {hackathon.prize}₹+</p>
+        <div className={cn("my-10")}>
+          <div className='bg-foreground/5 border border-foreground/10 rounded-lg px-6 py-8 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            <div className='col-span-1 lg:col-span-2'>
+              <HackathonStatus
+                className='w-fit'
+                startDate={hackathon.startDate}
+                endDate={hackathon.endDate}
+              />
+              <h1 className='text-4xl md:text-6xl font-bold mt-6'>
+                {hackathon.title}
+              </h1>
+              <p className='text-base md:text-lg text-foreground/90 font-medium leading-snug mt-4 tracking-wide'>
+                {hackathon.description}
+              </p>
+              <Link
+                href={`/hackathons/${hackathon.slug}/submit`}
+                className={cn(
+                  buttonVariants({ variant: "outline" }),
+                  "mt-4 md:mt-6"
+                )}
+              >
+                Participate in hackathon
+              </Link>
+            </div>
+            <div className='bg-background p-4 md:p-6'>
+              <HackathonDate
+                startDate={hackathon.startDate}
+                endDate={hackathon.endDate}
+              />
+              <hr className='my-5 border-foreground/10' />
+              <p className='flex gap-4 items-end'>
+                Prizes worth:{" "}
+                <span className='text-2xl md:text-3xl font-title font-medium'>
+                  {hackathon.prize}₹+
+                </span>
+              </p>
+            </div>
           </div>
           {/* MDX File information renders from here */}
           <div className=''>
--- src/components/hackathon-content/HackathonDate.tsx
+++ src/components/hackathon-content/HackathonDate.tsx
@@ -21,13 +21,23 @@ const HackathonDate = ({
   };
 
   return (
-    <div className={cn(className, "flex justify-between")} {...props}>
-      <div className="flex gap-4 items-center">
-        <CalendarIcon className="size-5" />
-        {new Date(startDate).toLocaleString("en-GB", options)} -{" "}
-        {new Date(endDate).toLocaleString("en-GB", options)}
+    <div className={cn(className, "")} {...props}>
+      <div className=''>
+        <CalendarIcon className='size-8 text-foreground/70' />
+        <p className='mt-4'>
+          {new Date(startDate).toLocaleString("en-GB", options)} -{" "}
+          {new Date(endDate).toLocaleString("en-GB", options)}
+        </p>
       </div>
-      <p className="">
+      <p className='border border-foreground/10 rounded-full text-sm px-3 py-2 flex justify-center items-center w-fit gap-1 md:gap-2 mt-4'>
+        <div
+          className={cn(
+            "w-4 h-4 rounded-full",
+            status == "ongoing" && "bg-[#23a394]",
+            status == "upcoming" && "bg-[#ee5b24]",
+            status == "ended" && "bg-foreground/10"
+          )}
+        ></div>
         {status == "upcoming" &&
           `Hackathon starts in ${timeDifference.daysStartToNow} days and ${timeDifference.hoursStartToNow} hours`}
 
