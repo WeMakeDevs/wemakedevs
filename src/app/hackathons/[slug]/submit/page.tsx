fd781619070457210b8d2e893a7c19bbfacf4e23
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-07 00:51:57 +0530

    fix: use consistent button styling

--- src/app/hackathons/[slug]/submit/page.tsx
+++ src/app/hackathons/[slug]/submit/page.tsx
@@ -7,53 +7,55 @@ import { fetchHackathonData } from "@/lib/hackathon";
 import { notFound } from "next/navigation";
 import { HackathonContentTitle } from "@/components/hackathon-content";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 const HackathonSubmit = async ({ params }: { params: { slug: string } }) => {
   const hackathons = await fetchHackathonData();
   const hackathon = hackathons.find(
-    (hackathon) => hackathon.slug == params.slug,
+    (hackathon) => hackathon.slug == params.slug
   );
 
   if (!hackathon) notFound();
 
   return (
-    <div>
+    <div className='mb-10'>
       <HackathonCoverImage src={hackathon.image.cover} alt={hackathon.title} />
-      <HackathonNav slug={hackathon.slug} page="submit" />
+      <HackathonNav slug={hackathon.slug} page='submit' />
       <ViewContainer>
         <HackathonContentTitle>
           Registration and Submission
         </HackathonContentTitle>
-        <div className="mb-4">
-          <h3 className="text-lg md:text-xl font-bold mb-2">Registration</h3>
-          <p className="text-foreground/80">
+        <div className='mb-4'>
+          <h3 className='text-lg md:text-xl font-bold mb-2'>Registration</h3>
+          <p className='text-foreground/80'>
             Please ensure you register before the deadline to participate.
           </p>
           <Link
             href={hackathon.registrationLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block px-6 py-4 mt-4 font-medium bg-blue-600 hover:bg-blue-800 w-fit"
+            target='_blank'
+            rel='noopener noreferrer'
+            className={cn(buttonVariants(), "mt-4 w-fit block")}
           >
             Register Now
           </Link>
         </div>
         <div>
-          <h3 className="text-lg md:text-xl font-bold mb-2 mt-12">
+          <h3 className='text-lg md:text-xl font-bold mb-2 mt-12'>
             Submission
           </h3>
-          <p className="text-foreground/80 mb-2">
+          <p className='text-foreground/80 mb-2'>
             Please read the{" "}
             <a
               href={`/hackathons/${hackathon.slug}/rules`}
-              className="bg-yellow-100/20 inline px-1 py-0.5 hover:bg-yellow-100/0"
+              className='bg-yellow-100/20 inline px-1 py-0.5 hover:bg-yellow-100/0'
             >
               submission rules
             </a>{" "}
             and{" "}
             <a
               href={`/hackathons/${hackathon.slug}/#requirements`}
-              className="bg-yellow-100/20 inline px-1 py-0.5 hover:bg-yellow-100/0"
+              className='bg-yellow-100/20 inline px-1 py-0.5 hover:bg-yellow-100/0'
             >
               submission requirements
             </a>{" "}
@@ -62,14 +64,14 @@ const HackathonSubmit = async ({ params }: { params: { slug: string } }) => {
           {hackathon.submissionLink ? (
             <Link
               href={hackathon.submissionLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block px-6 py-4 mt-4 font-medium bg-blue-600 hover:bg-blue-800 w-fit"
+              target='_blank'
+              rel='noopener noreferrer'
+              className={cn(buttonVariants(), "mt-4 w-fit block")}
             >
               Submit Now
             </Link>
           ) : (
-            <p className="text-foreground/80">
+            <p className='text-foreground/80'>
               Submissions will open soon. Stay tuned for updates!
             </p>
           )}
--- src/components/hackathon-content/HackathonNav.tsx
+++ src/components/hackathon-content/HackathonNav.tsx
@@ -2,6 +2,7 @@ import { HackathonNavInterface } from "@/types";
 import { ViewContainer } from "../ui/view-container";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { buttonVariants } from "../ui/button";
 
 const HackathonNav = ({
   page,
@@ -11,21 +12,21 @@ const HackathonNav = ({
 }: HackathonNavInterface) => {
   return (
     <nav
-      aria-label="secondary nav"
+      aria-label='secondary nav'
       className={cn(
         className,
-        "bg-foreground/5 border-t border-b border-foreground/10",
+        "bg-foreground/5 border-t border-b border-foreground/10"
       )}
       {...props}
     >
-      <ViewContainer className="flex justify-between flex-col md:flex-row">
-        <ul className="flex flex-col md:flex-row list-none">
+      <ViewContainer className='flex justify-between flex-col md:flex-row'>
+        <ul className='flex flex-col md:flex-row list-none'>
           <li>
             <Link
               href={`/hackathons/${slug}`}
               className={cn(
                 "px-6 py-4 font-medium hover:bg-foreground/10 inline-block",
-                page == "overview" && "shadow-secondaryNav",
+                page == "overview" && "shadow-secondaryNav"
               )}
             >
               Overview
@@ -36,7 +37,7 @@ const HackathonNav = ({
               href={`/hackathons/${slug}/resources`}
               className={cn(
                 "px-6 py-4 font-medium hover:bg-foreground/10 inline-block",
-                page == "resources" && "shadow-secondaryNav",
+                page == "resources" && "shadow-secondaryNav"
               )}
             >
               Resources
@@ -47,7 +48,7 @@ const HackathonNav = ({
               href={`/hackathons/${slug}/rules`}
               className={cn(
                 "px-6 py-4 font-medium hover:bg-foreground/10 inline-block",
-                page == "rules" && "shadow-secondaryNav",
+                page == "rules" && "shadow-secondaryNav"
               )}
             >
               Rules
@@ -58,7 +59,7 @@ const HackathonNav = ({
               href={`/hackathons/${slug}/projects`}
               className={cn(
                 "px-6 py-4 font-medium hover:bg-foreground/10 inline-block",
-                page == "projects" && "shadow-secondaryNav",
+                page == "projects" && "shadow-secondaryNav"
               )}
             >
               Projects
@@ -67,9 +68,7 @@ const HackathonNav = ({
         </ul>
         <Link
           href={`/hackathons/${slug}/submit`}
-          className={cn(
-            "px-6 py-4 font-medium bg-blue-600 hover:bg-blue-800 inline-block ml-auto",
-          )}
+          className={cn(buttonVariants(), "inline-block ml-auto")}
         >
           Submit Project
         </Link>
