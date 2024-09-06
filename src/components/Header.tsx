e1479b22cad66b87b189149a3bb7f61c14a97d20
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-07 00:37:25 +0530

    fix: use new button styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,41 +10,41 @@ import { ViewContainer } from "./ui/view-container";
 const Header = ({ className, ...props }: GeneralComponent) => {
   return (
     <header {...props} className={cn(className, "py-10 lg:py-14")}>
-      <ViewContainer className="text-center">
+      <ViewContainer className='text-center'>
         <Image
           src={heroBg}
-          alt="Hero background gradient"
-          className="absolute -top-[100px] w-[100%] sm:-top-[200px] sm:w-[80%] md:w-[60%] md:-top-[250px] max-w-[750px] left-1/2 -translate-x-1/2 opacity-40 -z-10"
+          alt='Hero background gradient'
+          className='absolute -top-[100px] w-[100%] sm:-top-[200px] sm:w-[80%] md:w-[60%] md:-top-[250px] max-w-[750px] left-1/2 -translate-x-1/2 opacity-40 -z-10'
         />
-        <div className="absolute -top-[250px] left-1/2 -translate-x-1/2 w-[300px] h-[300px] rounded-full blur-[150px] bg-primary"></div>
-        <p className="font-title text-xl md:text-2xl text-primary font-semibold">
+        <div className='absolute -top-[250px] left-1/2 -translate-x-1/2 w-[300px] h-[300px] rounded-full blur-[150px] bg-primary'></div>
+        <p className='font-title text-xl md:text-2xl text-primary font-semibold'>
           WeMakeDevs
         </p>
-        <h1 className="font-title text-4xl sm:text-5xl lg:text-7xl font-bold tracking-wide leading-tight mt-8">
+        <h1 className='font-title text-4xl sm:text-5xl lg:text-7xl font-bold tracking-wide leading-tight mt-8'>
           Hackathons for <br />
           {"{"}EVERYONE{"}"}
         </h1>
-        <p className="max-w-2xl w-full text-sm md:text-lg lg:text-lg mx-auto mt-10">
+        <p className='max-w-2xl w-full text-sm md:text-lg lg:text-lg mx-auto mt-10'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eos
           similique perspiciatis nam? Ratione numquam sapiente accusamus
           consectetur, debitis reprehenderit eos delectus similique
           perspiciatis.
         </p>
-        <div className="mx-auto flex flex-wrap gap-5 md:gap-10 justify-center mt-12">
+        <div className='mx-auto flex flex-wrap gap-5 md:gap-10 justify-center mt-12'>
           <Link
-            href="#hackathons"
+            href='#hackathons'
             className={cn(
-              buttonVariants({ variant: "default", size: "lg" }),
-              "text-base flex-auto sm:flex-initial sm:w-auto w-full",
+              buttonVariants({ variant: "default" }),
+              "text-base flex-auto sm:flex-initial sm:w-auto w-full"
             )}
           >
             Hackathons
           </Link>
           <Link
-            href="https://discord.gg/wemakedevs/"
+            href='https://discord.gg/wemakedevs/'
             className={cn(
-              buttonVariants({ variant: "ghost", size: "lg" }),
-              "text-base flex gap-2 flex-auto sm:flex-initial sm:w-auto w-full",
+              buttonVariants({ variant: "outline" }),
+              "text-base flex gap-2 flex-auto sm:flex-initial sm:w-auto w-full"
             )}
           >
             Join community <SiDiscord size={20} />
