e3dd637ce0a03ccec0e8c8a87272f9f3a513d1e9
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-09-10 20:46:05 +0530

    fix: console errors of countup remove
    add countUpRef component in span to remove those errors
    no change in the ui or working

--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -45,31 +45,28 @@ const Numbers = ({ className, ...props }: GeneralComponent) => {
 
   return (
     <section className={cn(className, "my-16")} {...props}>
-      <ViewContainer className="relative">
-        <h2 className="text-center text-4xl md:text-5xl">
-          Numbers say it all<span className="text-primary">_</span>
+      <ViewContainer className='relative'>
+        <h2 className='text-center text-4xl md:text-5xl'>
+          Numbers say it all<span className='text-primary'>_</span>
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 mt-12">
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 mt-12'>
           {numbers.map((item, index) => (
             <Link
               href={item.href}
-              className="bg-foreground/[.02] border border-foreground/10 rounded-xl p-5 relative group hover:scale-110 transition-transform hover:rotate-3"
+              className='bg-foreground/[.02] border border-foreground/10 rounded-xl p-5 relative group hover:scale-110 transition-transform hover:rotate-3'
               key={index}
             >
               <div
                 className={`absolute top-1/2 left-1/2 translate-x-1/2 -translate-y-1/2 w-[0px] h-[0px] blur-[70px] rounded-full group-hover:w-[50px] group-hover:h-[50px] transition-all`}
                 style={{ backgroundColor: item.color }}
               ></div>
-              <item.icon className="opacity-80" size={28} />
-              <p className="text-4xl md:text-5xl font-title font-medium mt-12 md:mt-20">
-                <CountUp
-                  end={item.number}
-                  start={0}
-                  suffix="+"
-                  enableScrollSpy
-                />
+              <item.icon className='opacity-80' size={28} />
+              <p className='text-4xl md:text-5xl font-title font-medium mt-12 md:mt-20'>
+                <CountUp end={item.number} start={0} suffix='+' enableScrollSpy>
+                  {({ countUpRef }) => <span ref={countUpRef} />}
+                </CountUp>
               </p>
-              <h3 className="font-body font-medium mt-3">{item.name}</h3>
+              <h3 className='font-body font-medium mt-3'>{item.name}</h3>
             </Link>
           ))}
         </div>
