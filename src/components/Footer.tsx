c324e014d1984c3561c151c246bea86c44b875db
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-10-16 23:05:28 +0530

    feat: revamp footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -57,31 +57,34 @@ const Footer = ({ className, ...props }: GeneralComponent) => {
     <footer
       className={cn(
         className,
-        "border-t border-foreground/25 py-12 bg-black/10",
+        "border-t border-foreground/25 py-12 bg-[#112130] relative"
       )}
       {...props}
     >
+      {/* <div className='w-full h-36 absolute left-0 top-0 -translate-y-full bg-gradient-to-t from-white to-background -z-10'></div> */}
       <ViewContainer>
-        <div className="flex items-center  flex-wrap gap-10 justify-between">
-          <div className="flex items-center gap-4">
-            <Image src={logo} alt="logo" className="w-12" />
-            <span className="text-xl font-bold font-title">WeMakeDevs</span>
+        <div className='flex items-center flex-wrap gap-10 justify-between'>
+          <div className='flex items-center gap-4'>
+            <Image src={logo} alt='logo' className='w-12' />
           </div>
-          <ul className="flex gap-4 md:gap-6 list-none">
+          <ul className='flex gap-4 md:gap-6 list-none'>
             {footerLinks.map((link) => (
               <li key={link.name}>
                 <Link
                   href={link.href}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="text-foreground/60 hover:text-foreground transition-colors"
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  className='text-white hover:text-background transition-colors'
                 >
-                  <link.icon className="w-5 h-5" />
+                  <link.icon className='w-5 h-5' />
                 </Link>
               </li>
             ))}
           </ul>
         </div>
+        {/* <p className='text-[20vw] lg:text-[180px] text-center font-extrabold bg-clip-text bg-gradient-to-r from-[#5236e5] via-[#e6b6fa] to-[#fee4fd] text-transparent'>
+          wemakedevs
+        </p> */}
       </ViewContainer>
     </footer>
   );
