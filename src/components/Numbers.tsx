975f8f8d878565ce4a6b188833d8edceae7beea0
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-10-17 20:04:07 +0530

    feat: add join button and transition in number cards

--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -10,6 +10,7 @@ import {
   SiX,
 } from "@icons-pack/react-simple-icons";
 import CountUp from "react-countup";
+import { ArrowUpRight } from "lucide-react";
 
 const Numbers = ({ className, ...props }: GeneralComponent) => {
   const numbers: numbersType = [
@@ -57,21 +58,31 @@ const Numbers = ({ className, ...props }: GeneralComponent) => {
           {numbers.map((item, index) => (
             <Link
               href={item.href}
-              className='bg-white shadow-lg hover:shadow-xl transition-shadow border border-foreground/10 rounded-xl p-5 relative group overflow-clip'
+              className='group bg-white shadow-lg hover:shadow-xl transition-shadow border border-foreground/10 rounded-xl p-5 pb-4 relative group overflow-clip'
               key={index}
             >
               <div
                 className={`duration-300 z-0 absolute bottom-0 left-0 w-full h-full transition-transform origin-top group-hover:origin-bottom scale-y-0 group-hover:scale-y-100 bg-${item.color}`}
               ></div>
               <item.icon className='opacity-80 z-10' size={28} />
-              <p className='relative text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-title font-medium mt-12 md:mt-20 z-10'>
-                <CountUp end={item.number} start={0} suffix='+' enableScrollSpy>
-                  {({ countUpRef }) => <span ref={countUpRef} />}
-                </CountUp>
+              <div className='mt-16 md:mt-24 group-hover:-translate-y-8 transition-transform'>
+                <h3 className='font-body font-medium relative z-10'>
+                  {item.name}
+                </h3>
+                <p className='relative text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-title font-medium mt-3 z-10'>
+                  <CountUp
+                    end={item.number}
+                    start={0}
+                    suffix='+'
+                    enableScrollSpy
+                  >
+                    {({ countUpRef }) => <span ref={countUpRef} />}
+                  </CountUp>
+                </p>
+              </div>
+              <p className='absolute bottom-0 left-5 translate-y-[22px] group-hover:-translate-y-2.5 transition-all flex gap-2 items-center font-title font-semibold text-lg md:text-xl opacity-75 mix-blend-multiply'>
+                Join <ArrowUpRight strokeWidth={2} />
               </p>
-              <h3 className='font-body font-medium mt-3 relative z-10'>
-                {item.name}
-              </h3>
             </Link>
           ))}
         </div>
