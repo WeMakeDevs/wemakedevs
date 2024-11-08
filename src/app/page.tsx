3dcc421819ae5b530933c7e76a10b459f7d72821
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-09 00:17:52 +0530

    fix(ui): make banner responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,20 +31,22 @@ export default function Home() {
       <Footer />
 
       <Banner className='flex items-center text-base md:text-lg font-medium'>
-        <span className=''>
-          Registrations for HackFrost Winter Hackathon are open!
-        </span>
-        <Link
-          href='/hackathons/hackfrost-hackathon'
-          className={cn(
-            buttonVariants(),
-            "mt-4 md:mt-0 md:ml-8 px-3 py-2 bg-white hover:bg-primary text-primary hover:text-white"
-          )}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Know more
-        </Link>
+        <div className='flex flex-col md:flex-row items-center justify-center'>
+          <span className=''>
+            Registrations for HackFrost Winter Hackathon are open!
+          </span>
+          <Link
+            href='/hackathons/hackfrost-hackathon'
+            className={cn(
+              buttonVariants(),
+              "mt-4 md:mt-0 md:ml-8 px-3 py-2 bg-white hover:bg-primary text-primary hover:text-white w-full md:w-fit"
+            )}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Know more!
+          </Link>
+        </div>
       </Banner>
     </div>
   );
