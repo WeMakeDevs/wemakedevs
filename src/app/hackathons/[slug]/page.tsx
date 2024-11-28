4baff8caa906afdd3b812742acfcdb9dcd8dcd38
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-28 19:15:38 +0530

    feat: add github link to kestra in prizes section

--- src/app/hackathons/[slug]/page.tsx
+++ src/app/hackathons/[slug]/page.tsx
@@ -13,7 +13,7 @@ import { ViewContainer } from "@/components/ui/view-container";
 import { HACKATHON_FAQS } from "@/constants";
 import { fetchHackathonData } from "@/lib/hackathon";
 import { cn } from "@/lib/utils";
-import { Star } from "lucide-react";
+import { ArrowUpRight, Star } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
@@ -86,35 +86,38 @@ const HackathonPage = async ({ params }: { params: { slug: string } }) => {
 									<HackathonContentTitleH3>
 										Side Quest
 									</HackathonContentTitleH3>
-									{hackathon.prizes.giveaway.map(prize => (
-										<div
-											className="space-y-4"
-											key={prize.name}
-										>
-											<p className="text-lg md:text-xl font-semibold flex gap-2 items-center">
-												<Star
-													size={20}
-													strokeWidth={0}
-													fill="#ffd333"
-													className="shrink-0"
-												/>{" "}
-												<div className="flex flex-wrap gap-2 items-center">
-													{prize.name}{" "}
-													{prize.prize && "-"}
-													{prize.prize && (
-														<span className="px-2 py-1 bg-blue-600 text-white rounded-[4px]">
-															{prize.prize}
-														</span>
-													)}
-												</div>
-											</p>
-											{prize.description && (
-												<p className="text-base md:text-lg font-medium">
-													{prize.description}
-												</p>
-											)}
-										</div>
-									))}
+									<div className="space-y-4">
+										<p className="text-lg md:text-xl font-semibold flex gap-2 items-center">
+											<Star
+												size={20}
+												strokeWidth={0}
+												fill="#ffd333"
+												className="shrink-0"
+											/>{" "}
+											<div className="flex flex-wrap gap-4 items-center">
+												<Link
+													href="https://github.com/kestra-io/kestra"
+													className="underline hover:no-underline group flex items-center gap-1"
+													target="_blank"
+													rel="noopener noreferrer"
+												>
+													Star Kestra on GitHub{" "}
+													<ArrowUpRight className="group-hover:-translate-y-1 group-hover:translate-x-1 transition-transform" />
+												</Link>{" "}
+												-
+												<span className="px-2 py-1 bg-blue-600 text-white rounded-[4px]">
+													Exclusive Swag Bag
+												</span>
+											</div>
+										</p>
+										<p className="text-base md:text-lg font-medium">
+											Top 20 people who star Kestra on
+											Github and share their hackathon
+											experience on socials using
+											#HackFrost2024 will get an exclusive
+											swag bag.
+										</p>
+									</div>
 								</>
 							)}
 						</div>
