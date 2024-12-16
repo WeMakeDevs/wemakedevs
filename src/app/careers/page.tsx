7831c34b084e1934a53545fe70a2bcdb8e6f95f4
Aayush Sharma <aayusharma1401@gmail.com>
2024-12-16 15:57:58 +0530

    chore: updated careers page content

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -62,8 +62,7 @@ const Careers = () => {
 						<span className="text-primary">WeMakeDevs</span>
 					</h1>
 					<p className="mt-7 text-sm sm:text-base max-w-3xl font-medium sm:leading-snug">
-						If you someone who is interested to work for the below
-						roles and get mentorship from industry leaders, apply
+						If you're looking for a paid opportunity with mentorship from industry leaders, apply
 						now by emailing us at{" "}
 						<Link
 							href="mailto:contact@wemakedevs.org"
@@ -74,7 +73,7 @@ const Careers = () => {
 						>
 							contact@wemakedevs.org
 						</Link>{" "}
-						with your updated CV and proof of work attached to it.
+						with your updated resume.
 					</p>
 					<div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-10">
 						{jobs.map(job => (
