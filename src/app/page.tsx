841d99d7a40d806e9703df4d1ba49bcdc86d61af
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2026-01-15 18:21:52 +0000

    Comment out Banner component in page.tsx
    Comment out the Banner component in the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 			<Partners />
 			<Footer />
 
-			<Banner className="flex items-center text-base md:text-lg font-medium">
+			{/* <Banner className="flex items-center text-base md:text-lg font-medium">
 				<div className="flex flex-col md:flex-row items-center justify-center">
 					<span className="text-white">
 						Backend Reloaded Hackathon is live! $3000+ in cash
@@ -50,7 +50,7 @@ export default function Home() {
 						Register Now!
 					</Link>
 				</div>
-			</Banner>
+			</Banner> */}
 		</div>
 	);
 }
