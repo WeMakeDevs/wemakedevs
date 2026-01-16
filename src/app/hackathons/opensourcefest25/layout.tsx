08d13c40df0236c12dcd5f1311d5b1eeed0580a9
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2026-01-16 10:58:05 +0000

    kestra

--- src/app/hackathons/opensourcefest25/layout.tsx
+++ src/app/hackathons/opensourcefest25/layout.tsx
@@ -34,11 +34,6 @@ export default function RootLayout({
 			url: `/hackathons/${DATA.slug}#faq`,
 			type: "link",
 		},
-		{
-			name: DATA.cta.label,
-			url: DATA.cta.href,
-			type: "button",
-		},
 	];
 
 	return (
