a16111c332048d60ea6f26593353fd58b6b1eb08
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2026-01-23 02:11:30 +0000

    about change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
 		<div>
 			<Navbar />
 			<Header />
+			<Numbers />
 			<Testimonials />
 			<Newsletter />
-			<Numbers />
 			<Hackathons />
 			<Meetups />
 			<Initiatives />
