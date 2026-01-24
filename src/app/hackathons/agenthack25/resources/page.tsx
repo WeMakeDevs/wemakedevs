cde39e6973b64cd1049d7cfd1cfe9c7d72fdde70
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2026-01-24 13:29:20 +0000

    Ai Agents winners

--- src/app/hackathons/agenthack25/resources/page.tsx
+++ src/app/hackathons/agenthack25/resources/page.tsx
@@ -17,12 +17,6 @@ const HackathonResources = async () => {
 				slug={DATA.slug}
 				page="resources"
 				links={navLinks}
-				navCta={{
-					label: DATA.cta.label,
-					href: DATA.cta.href,
-					openInNewTab: DATA.cta.openInNewTab,
-					disabled: DATA.cta.disabled,
-				}}
 			/>
 			<ViewContainer>
 				<HackathonContentBody>
