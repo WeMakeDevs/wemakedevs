608bcf52a1c8079d24511215a863c76f0fdc4312
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2026-01-27 17:09:52 +0000

    hack all feb

--- src/components/Hackathons.tsx
+++ src/components/Hackathons.tsx
@@ -78,7 +78,7 @@ const Hackathons = ({ className, ...props }: GeneralComponent) => {
 									</div>
 									<div className="flex items-center gap-2 text-sm text-gray-300">
 										<Smartphone className="w-4 h-4 text-accent-4" />
-										<span>Galaxy Flip 7 Grand Prize</span>
+										<span>Galaxy Flip 7 Raffle</span>
 									</div>
 								</div>
 								<span
