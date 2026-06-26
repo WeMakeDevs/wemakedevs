0eb8c408acd8b6415ccb2fb2fddf59ab069054ab
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2026-06-26 14:22:24 -0400

    meetups

--- src/app/meetups/images/index.ts
+++ src/app/meetups/images/index.ts
@@ -1,7 +1,7 @@
 import brusselsAiMixer from "./brussels-june-26.jpg";
 import openSourceConnectBangaloreMeetup from "./open-source-connect-bangalore.jpg";
 import openSourceConnectMeetup from "./open-source-connect.jpg";
-import sfAiEngineerHappyHour from "./sf-july-26.jpg";
+import sfAiEngineerHappyHour from "./sf-july-26.png";
 
 export {
 	brusselsAiMixer,
